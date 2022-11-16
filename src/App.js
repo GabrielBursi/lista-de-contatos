7a21fb8044bd9fa1b959291650fecb9605f20fb0
Gabriel Bursi <gabrielcavalarosuporte@gmail.com>
2022-11-16 15:34:35 -0300

    verificar contato repetido

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState,useRef } from 'react';
 import ListaDeContato from './components/ListaDeContato';
 
 function App() {
 
   const [contato, setContato] = useState({nome: '', tel: ''});
   const [listaContato, setListaContato] = useState([]);
+
+  const nomeRef = useRef();
+  const telRef = useRef()
 
   function definirNome(e){
     setContato({...contato, nome: e.target.value})
@@ -18,7 +21,16 @@ function App() {
 
     if(contato.nome === '' || contato.tel === '') return
 
+    const contatoDuplicado = listaContato.find(ct => ct.nome === contato.nome && ct.tel === contato.tel)
+    if(contatoDuplicado){
+      telRef.current.focus()
+      return
+    }
+
     setListaContato([...listaContato,contato])
+
+    setContato({nome:'', tel:''})
+    nomeRef.current.focus()
   }
 
   return (
@@ -27,11 +39,11 @@ function App() {
       <hr />
       <div>
         <label>Nome:</label>
-        <input type="text" onChange={definirNome}></input>
+        <input type="text" ref={nomeRef} onChange={definirNome} value={contato.nome}></input>
       </div>
       <div>
         <label>Telefone:</label>
-        <input type="tel" required placeholder="(xx) xxxxx-xxxx" onChange={definirTel}></input>
+        <input type="tel" ref={telRef} required placeholder="(xx) xxxxx-xxxx" onChange={definirTel} value={contato.tel}></input>
       </div>
       <button onClick={adicionarContato}>Adicionar contato</button>
       <hr/>
