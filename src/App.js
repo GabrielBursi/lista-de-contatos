cc123cf1e1dfd10c735c0e4f5805f7f3796829d1
Gabriel Bursi <gabrielcavalarosuporte@gmail.com>
2022-11-16 15:05:58 -0300

    função para criar a lista de contatos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import { v4 as id } from "uuid";
 
 function App() {
+
+  const [contato, setContato] = useState({nome: '', tel: ''});
+  const [listaContato, setListaContato] = useState([]);
+
+  function definirNome(e){
+    setContato({...contato, nome: e.target.value})
+  }
+
+  function definirTel(e) {
+    setContato({ ...contato, tel: e.target.value });
+  }
+
+  function adicionarContato(){
+    setListaContato([...listaContato,contato])
+  }
+
   return (
     <>
-
+      <h1>Lista de contatos</h1>
+      <hr />
+      <div>
+        <label>Nome:</label>
+        <input type="text" onChange={definirNome}></input>
+      </div>
+      <div>
+        <label>Telefone:</label>
+        <input type="number" onChange={definirTel}></input>
+      </div>
+      <button onClick={adicionarContato}>Adicionar contato</button>
+      <hr/>
+      {listaContato.map(ct => (
+        <ul>
+          <li key={id()}>Nome: {ct.nome} Telefone: {ct.tel}</li>
+        </ul>
+      ))}
     </>
   );
 }
